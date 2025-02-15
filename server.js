c03501edb97ecf82133dbdd1e09d4c768ddc1f50
Jhietechz <jhietech0@gmail.com>
2025-02-15 23:32:08 +0300

    Update server.js

--- server.js
+++ server.js
@@ -14,7 +14,8 @@ const db = mysql.createConnection({
     user: "process.env.DB_USER",
     password: "process.env.DB_PASS",
     database: "process.env.DB_NAME",
-    port:"3306"
+    port: "process.env.DB_PORT",
+    
 });
 
 db.connect((err) => {
