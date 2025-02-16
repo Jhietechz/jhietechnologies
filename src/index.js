40526e7b7aaf74a79b555ea740c343974247d124
Jhietechz <jhietech0@gmail.com>
2025-02-16 17:52:36 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -11,7 +11,8 @@ root.render(
     <App />
      
   </React.StrictMode>
-);reportWebVitals();
+);
+reportWebVitals();
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
