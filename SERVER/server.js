1310d3fac2ec8808ab6fce005a677e829c6570fc
Jhietechz <jhietech0@gmail.com>
2025-02-16 12:14:56 +0300

    Update server.js

--- SERVER/server.js
+++ SERVER/server.js
@@ -1,8 +1,14 @@
 const mysql = require("mysql");
 const db_url = new 
     URL(process.env.DB_URL);
-const express = require("express");
 const cors = require("cors");
+const express = require("express");
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+
+app.listen(PORT, () => console.log('Server running on port ${PORT}'));
+
 app.use(cors({origin: "https://jhietechnologies.netlify.app", credentials: true}));
 app.use((req, res, next) => {
     res.header("Access-Control-Allow-Origin", "https://your-netlify-site.netlify.app");
