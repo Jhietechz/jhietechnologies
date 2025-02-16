dfe6200ea4cdece00fb7c1e217c0c36e931c009e
Jhietechz <jhietech0@gmail.com>
2025-02-16 16:36:57 +0300

    Update server.js

--- SERVER/server.js
+++ SERVER/server.js
@@ -10,6 +10,20 @@ app.use(cors({origin: "https://jhietechnologies.netlify.app",
               credentials: true}));
 app.listen(PORT, () => console.log('Server running on port ${PORT}'));
 
+const allowedOrigins = ['https://jhietechnologies.netlify.app'];
+   app.use(cors({
+     origin: function(origin, callback){
+      // allow requests with no origin 
+      // (like mobile apps or curl requests)
+      if(!origin) return callback(null, true);
+      if(allowedOrigins.indexOf(origin) === -1){
+        var msg = 'The CORS policy for this site does not ' +
+                  'allow access from the specified Origin.';
+        return callback(new Error(msg), false);
+      }
+      return callback(null, true);
+    }
+   }));
 
 app.use((req, res, next) => {
     res.header("Access-Control-Allow-Origin", "https://jhietechnologies.netlify.app");
